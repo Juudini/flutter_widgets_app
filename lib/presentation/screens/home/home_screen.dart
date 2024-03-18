de535c9913ac7cb2e06ee7085e872cef0f727128
Juudini <juudinidev@gmail.com>
2024-03-17 22:31:00 -0300

    feat: finished buttons screen

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_widgets_app/config/menu/menu_items.dart';
-import 'package:flutter_widgets_app/presentation/screens/cards/cards_screen.dart';
 import 'package:go_router/go_router.dart';
 
 class HomeScreen extends StatelessWidget {
