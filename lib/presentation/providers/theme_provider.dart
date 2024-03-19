5cd1df0ad1a11f8926433b0efb76e15a32ef5ead
Juudini <juudinidev@gmail.com>
2024-03-19 17:59:26 -0300

    feat: added theme app

--- lib/presentation/providers/theme_provider.dart
+++ lib/presentation/providers/theme_provider.dart
@@ -1,3 +1,24 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_widgets_app/config/theme/app_theme.dart';
 
-final isDarkModeProvider = StateProvider<bool>((ref) => false);
+final colorListProvider = Provider((ref) => colorList);
+
+final isDarkmodeProvider = StateProvider<bool>((ref) => false);
+
+final selectedColorProvider = StateProvider<int>((ref) => 0);
+
+final themeNotifierProvider = StateNotifierProvider<ThemeNotifier, AppTheme>(
+  (ref) => ThemeNotifier(),
+);
+
+class ThemeNotifier extends StateNotifier<AppTheme> {
+  ThemeNotifier() : super(AppTheme());
+
+  void toggleDarkmode() {
+    state = state.copyWith(isDarkmode: !state.isDarkmode);
+  }
+
+  void changeColorIndex(int colorIndex) {
+    state = state.copyWith(selectedColor: colorIndex);
+  }
+}
